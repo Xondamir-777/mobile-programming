d82edce44ad376183d4b76794b8424e50228701d
Xondamir-777 <150456608+Xondamir-777@users.noreply.github.com>
2025-09-03 22:20:55 +0500

    Update q12.cpp

--- q12.cpp
+++ q12.cpp
@@ -1,50 +1,45 @@
-//Monday 14:00
+//wensday 9:00
 #include <iostream>
-#include <stdexcept> // For std :: invalid_argument
+#include <stdexcept>
+
+using namespace std;
 
 void processAge ( int age ) {
     if ( age < 0) {
-        throw std :: invalid_argument (" Age cannot be negative .") ;
+        throw invalid_argument (" Age cannot be negative .") ;
     }
-    std :: cout << " Age processed successfully : " << age << std :: endl ;
+    cout << " program was executed corrctly : " << age << endl ;
 }
 
 int main () {
-    int userAge = -5;
+    int age = -10;
     try {
-        std :: cout << " Processing age: " << userAge << std :: endl ;
-        processAge ( userAge ) ;
+        cout  << age << endl ;
+        processAge ( age ) ;
     }
-    catch ( const std :: invalid_argument & e ) {
-        // Catches the specific exception type
-        std :: cerr << " Error : " << e . what () << std :: endl ;
+    catch ( const invalid_argument & e ) {
+        cerr << " Error : " << e . what () << endl ;
     }
-
-    std :: cout << " Program continues ." << std :: endl ;
     return 0;
 }
 
 /*
 Q2:
-try → wraps code that may throw.
-
-catch → handles the exception.
-
-finally (Java) → always runs cleanup code.
+try - executes code if there is error , it stops execution of code but it doesn't stop program itself.
+catch - logic that has to be done if there is type of error that it has to chatch.
+finally - always runs cleanup code.
 
 C++ → no finally. Instead uses RAII → resources (files, locks, memory) are tied 
 to objects that release them automatically when they go out of scope (destructor).
 
 
 Q3. 
-Checked → must be declared/handled (e.g. IOException).
-Unchecked → runtime errors, not enforced (e.g. NullPointerException).
-C++ → doesn’t enforce this distinction. All exceptions are unchecked.
+Checked - must behandled.
+Unchecked - runtime errors, not enforced.
+C++: doesn’t enforce this distinction. All exceptions are unchecked.
 
 Q4.
-Advantages: cleaner code (no scattered error checks), separates error logic from 
-normal logic, propagates errors automatically.
+Advantages: cleaner code, separates error logic from normal logic, propagates errors automatically.
 
-Disadvantages: runtime overhead, harder to predict control flow, misuse can make 
-debugging harder, not ideal for performance-critical inner loops.
+Disadvantages: runtime overhead, harder to predict control flow, misuse can make debugging harder, not ideal for performance-critical inner loops.
 */
