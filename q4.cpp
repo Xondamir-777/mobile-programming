0c712dd6d323c036a07bf77a35c15b5215a9a50a
Xondamir-777 <150456608+Xondamir-777@users.noreply.github.com>
2025-09-03 22:15:10 +0500

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,10 +1,16 @@
+//wensday 9:00
 #include <iostream>
 # include <string>
-# include <algorithm> // For std :: equal
+# include <algorithm>
 
 bool isPalindrome ( const std :: string & str ) {
-// An elegant way is to compare the first half with the reverse of the second half .
-return std :: equal ( str . begin () , str . begin () + str . size () /2 , str . rbegin () ) ;
+    int l = str.length();
+    for (int i=0; i<l; i++){
+        if(str[i]!=str[l-1-i]){
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main () {
@@ -12,3 +18,26 @@ std :: cout << " madam is palindrome ? " << ( isPalindrome (" madam ") ? " true 
 std :: cout << " hello is palindrome ? " << ( isPalindrome (" hello ") ? " true " : " false") << std :: endl ;
 return 0;
 }
+
+
+/*
+Q2:In C++, we can use Pass-by-value, Pass-by-pointer, Pass-by-reference:
+Pass-by-value. The function receives a copy of the argument. Changes within the function do not affect the original variable.
+Pass-by-pointer. The function receives the address of the variable. It is possible to change the contents by address.
+Pass-by-reference. The function receives a reference to the variable. The changes affect the original.
+
+In Java Always pass-by-value:
+For primitives, the value itself is copied.
+For objects, the address is copied, not the object itself.
+
+Q3: Overloading is the ability to have multiple functions/methods with the same name but different parameters.
+The return type of function or input parameters must be different.
+
+Q4:Recursion is when a function calls itself to solve a problem.
+Base case is condition to stop function, if there is no base case it runs untill stack overflow
+
+
+
+
+
+*/
