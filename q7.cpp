efb4b679962980a8cf2f3a29b7853be373e25774
Xondamir-777 <150456608+Xondamir-777@users.noreply.github.com>
2025-09-03 22:18:14 +0500

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,6 +1,10 @@
+
+//wensday 9:00
 #include <iostream>
 
 #include <string>
+
+using namespace std;
 
 enum class Season {
     SPRING ,
@@ -8,20 +12,36 @@ enum class Season {
     AUTUMN ,
     WINTER
 };
-std :: string recommendActivity ( Season currentSeason ) {
+string recommendActivity ( Season currentSeason ) {
     switch ( currentSeason ) {
-        case Season :: SPRING : return " Plant flowers ";
-        case Season :: SUMMER : return "Go to the beach ";
-        case Season :: AUTUMN : return " Rake leaves ";
-        case Season :: WINTER : return " Build a snowman ";
+        case Season :: SPRING : return " Be ready to exams ";
+        case Season :: SUMMER : return "Relax";
+        case Season :: AUTUMN : return " Prepare to 1 september ";
+        case Season :: WINTER : return " Be ready to exams ";
         default : return " Rest ";
     }
 }
 
 int main () {
-    Season now = Season :: AUTUMN ;
-    std :: cout << "It ’s autumn ! Let ’s: " << recommendActivity ( now ) << std :: endl ;
+    Season now = Season :: SUMMER ;
+    cout << "Finally It ’s summer ! Time to  " << recommendActivity ( now ) << endl ;
 
     return 0;
 
 }
+
+
+/*
+Q2: Easy to read, safety: doesn't read if text include typo
+
+
+Q3:
+Enums ensure only valid values can be used, making code safer and easier to read. 
+
+Q4:
+C++ scoped enums (enum class) are type-safe but limited: they hold named constants.
+Java enums are more powerful: they can have fields, methods, and constructors, acting 
+almost like special classes.
+Old C-style enums in C++ weren’t type-safe (could mix with ints), but scoped enums fix that.
+
+*/
