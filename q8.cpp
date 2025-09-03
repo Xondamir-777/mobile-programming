59d9078dc740e526c13deda31f8548bc9276f609
Xondamir-777 <150456608+Xondamir-777@users.noreply.github.com>
2025-09-03 22:18:31 +0500

    Update q8.cpp

--- q8.cpp
+++ q8.cpp
@@ -1,26 +1,45 @@
+//wensday 9:00
 #include <iostream>
 
-// Base class ( Superclass )
+using namespace std;
+
+
 class Vehicle {
     public :
-     void startEngine () {
-        std :: cout << " Engine has started ." << std :: endl ;
+    void startEngine () {
+        cout << " Engine is working! " << endl ;
     }
 };
 
-// Derived class ( Subclass )
+
 class Car : public Vehicle {
     public :
     void drive () {
-        std :: cout << " The car is moving ." << std :: endl ;
+        cout << " Let's go!" << endl ;
     }
 };
 
 int main () {
     Car myCar ;
-    myCar . startEngine () ; // Inherited from Vehicle
-    myCar . drive () ; // Defined in Car
+    myCar . startEngine () ;
+    myCar . drive () ; 
 
     return 0;
 
 }
+
+
+/*
+Q2: is-a → inheritance, used for generalization or specialization.
+has-a → composition, used for composite objects.
+
+Q3:
+A derived class provides its own version of a method from the base class.
+In C++, virtual in base and override in derived make overriding explicit.
+In Java, @Override ensures the method truly overrides.
+
+Q4: It happens when a class inherits from two classes that share the same base, causing ambiguity.
+C++ fixes it with virtual inheritance.
+Java avoids it because classes can’t have multiple inheritance, only multiple interfaces.
+
+*/
